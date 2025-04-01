468d8087f66593f2c82e90b2dec229e21166549f
Alex Zenchik <alexcomp2021@gmail.com>
2025-04-01 09:34:04 +0300

    Setup project + install dependencies

---
+++ src/main/java/org/example/yourstockv2backend/YourStockV2BackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.yourstockv2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourStockV2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourStockV2BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/yourstockv2backend/YourStockV2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.yourstockv2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourStockV2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
